eeb8eb3d04e1f1d282811499ebd22233154b2b3e
Joyce <gathiithijoyce74@gmail.com>
2025-07-23 11:00:43 +0300

    fixed bug in simple_file_encryptor.py

--- simple_file_encryptor_too.py
+++ simple_file_encryptor_too.py
@@ -2,11 +2,16 @@ print("Simple File Encyptor with  Caesar Cipher")
 
 filename=input("Enter the file name to encrypt: ")
 try:
-    with open(filename, 'r') as file:
-        content=file.read()
-        print(content)
+ with open(filename, 'r') as file:
+    content=file.read()
+    print(content)
+
+    encrypted_content = ceaser_cypher(content, 3)
+    print(encrypted_content)
+
 except FileNotFoundError:
     print("File not found. Please check the file name and try again.")
+
 def ceaser_cypher(text,shift):
     encrypted_text = ""
     for char in text:
@@ -16,5 +21,3 @@ def ceaser_cypher(text,shift):
         else:
             encrypted_text += char
     return encrypted_text
-encrypted_content = ceaser_cypher(content, 3)
-print(encrypted_content)
